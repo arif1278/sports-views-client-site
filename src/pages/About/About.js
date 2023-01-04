7217458861d2c4d04bddaa06410e2937918b26c4
arif1278 <manabethreeplusone@gmail.com>
2023-01-04 19:37:56 +0600

    last commit about

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import me from '../../assets/about.jpg'
 
 const About = () => {
     return (
@@ -6,7 +7,7 @@ const About = () => {
 
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="https://media.istockphoto.com/photos/young-woman-using-dslr-camera-picture-id610259354?b=1&k=20&m=610259354&s=612x612&w=0&h=v4PyNrpS60svYCZFm6N0Gunqufb2f8ggfIdsrjJzho0=" className="max-w-sm rounded-lg shadow-2xl" alt='' />
+                <img src={me} className="max-w-sm rounded-lg shadow-2xl" alt='' />
                 <div>
                     <h1 className="text-5xl font-bold">About my photography</h1>
                     <p className="py-6">More and more photographers have turned to social media to display their work and connect with clients. While this is convenient, you need to remember that not everyone is on social media. Plus, a professional photography website can do so much more for you than an Instagram or Facebook page—think organized galleries, SEO, even an online store for prints and merch!</p>
